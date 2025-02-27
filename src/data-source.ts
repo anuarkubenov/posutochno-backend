43861c106c2c208fa2f4a136edd5f83ee3cc1c4c
akubenov <anuar.kubenov@dnapayments.com>
2025-02-27 14:39:09 +0500

    data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,15 @@ import { Apartment } from './apartments/entities/apartment.entity';
 import { ApartmentPhoto } from './apartments/entities/apartment-photo.entity';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'BArcelona11229!',
-  database: 'apartments',
+  type: process.env.DB_TYPE as "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Apartment, ApartmentPhoto],
   migrations: [__dirname + '/migrations/*.ts'],
-  synchronize: false,
-  logging: true,
+  synchronize: true,
+  // synchronize: process.env.NODE_ENV === "development", // В продакшене FALSE
+  logging: process.env.NODE_ENV === "development",
 });
